a66fa014db465e3696905039a29e53b7868e5ecc
GMELUM <gmelum@my.com>
2023-02-20 21:23:23 +0300

    main

---
+++ index.d.ts
@@ -0,0 +1,52 @@
+import * as react from 'react';
+
+type TNextPage = (options: Partial<{
+    view: string;
+    panel: string;
+    modal: string;
+    popout: string;
+    stay: boolean;
+    freeze: boolean;
+    params: Record<string, string | number>;
+} & Record<string, any>>) => void;
+declare const nextPage: TNextPage;
+
+declare const backPage: () => void;
+
+declare const ACTIVE_VIEW: {
+    key: string;
+    default: string;
+    get: () => string;
+    set: (v: any) => void;
+    sub: (handle: react.Dispatch<string>) => void;
+};
+declare const ACTIVE_PANEL: {
+    key: string;
+    default: string;
+    get: () => string;
+    set: (v: any) => void;
+    sub: (handle: react.Dispatch<string>) => void;
+};
+declare const ACTIVE_MODAL: {
+    key: string;
+    default: string;
+    get: () => string;
+    set: (v: any) => void;
+    sub: (handle: react.Dispatch<string>) => void;
+};
+declare const ACTIVE_POPOUT: {
+    key: string;
+    default: string;
+    get: () => string;
+    set: (v: any) => void;
+    sub: (handle: react.Dispatch<string>) => void;
+};
+declare const ACTIVE_PARAMS: {
+    key: string;
+    default: Record<string, string | number>;
+    get: () => Record<string, string | number>;
+    set: (v: any) => void;
+    sub: (handle: react.Dispatch<Record<string, string | number>>) => void;
+};
+
+export { ACTIVE_MODAL, ACTIVE_PANEL, ACTIVE_PARAMS, ACTIVE_POPOUT, ACTIVE_VIEW, backPage, nextPage };

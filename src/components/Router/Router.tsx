bf2b873512aefaf29e04a7dae4c2d70a46396d8d
GMELUM <gmelum@my.com>
2023-04-18 06:39:29 +0300

    0.0.9

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect } from 'react';
 import { FC, Fragment, HTMLAttributes } from 'react';
 import { nextPage } from "../../";
+import { getter } from 'elum-state';
+import { ACTIVE_VIEW } from 'atoms';
 
 interface IRouter extends HTMLAttributes<HTMLDivElement> {
     branch: string;
@@ -10,7 +12,7 @@ const Router: FC<IRouter> = ({
     branch,
     children
 }) => {
-    useLayoutEffect(() => nextPage({ view: branch }), []);
+    if (!getter(ACTIVE_VIEW)) { nextPage({ view: branch }) }
     return (<Fragment>{children}</Fragment>);
 }
 
